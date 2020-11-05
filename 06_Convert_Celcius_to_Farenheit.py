6a1b82bdd235e2eff77bbd55dd761199a0a7a0f8
iaz <https://github.com/Step-basic-python/fundamental-Python>
2020-11-05 16:44:14 +0700

    Learning Python Fundamental

---
+++ 06_Convert_Celcius_to_Farenheit.py
@@ -0,0 +1,10 @@
+# Write a simple program to convert degree of Celcius to Farenheit
+
+# Prompt user for input degree in Celcius
+celcius = int(input('Enter a degree in Celcius : '))
+
+# Convert Celcius to Farenheit
+fahrenheit = (9/5) * celcius + 32
+
+# Display the result
+print(f"{celcius} Celcius is {fahrenheit} Fahrenheit")
