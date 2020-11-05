f654f4e35a52001fc1fd8fbce274f2c31c897e97
iaz <https://github.com/Step-basic-python/fundamental-Python>
2020-11-05 17:11:54 +0700

    Learning Python Fundamental

---
+++ 09_Convert_pounds_into_kilograms.py
@@ -0,0 +1,10 @@
+# Write a simple program to convert pounds into kilograms
+
+# Prompt user for input a value in pounds
+pounds = float(input('Enter a value in pounds : '))
+
+# Convert pounds into kilograms
+kilograms = pounds * 0.454
+
+# Display the result
+print(f"{pounds} pounds is {kilograms} kilograms")
