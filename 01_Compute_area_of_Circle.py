29ece43292ecac67c18fd70f373814c550a49420
Nehemia Sembiring <34985105+nehan90@users.noreply.github.com>
2020-11-05 12:22:03 +0700

    Learning Python Fundamental

---
+++ 01_Compute_area_of_Circle.py
@@ -0,0 +1,30 @@
+# Write a simple program to compute area of circle
+
+print('Example 01 :')
+# Make a Constant
+PHI = 3.14159
+
+# Assign a value to radius
+radius = 20
+
+# Compute area
+area = PHI * radius * radius
+
+# Display the result
+print(f"The area of circle with radius {radius} is {area} m3")
+
+print()
+print('Example 02 :')   # Reading input from the console
+                        # Use operator input
+
+# Make a Constant
+PHI = 3.14159
+
+# Assign a value to radius
+radius = int(input('Enter a value of radius : '))
+
+# Compute area
+area = PHI * radius * radius
+
+# Display the result
+print(f"The area of circle with radius {radius} is {area} m3")
