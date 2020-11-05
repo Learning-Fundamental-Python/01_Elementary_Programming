7fbc6f9272c41f8551d193fcd64b742354339f1a
iaz <https://github.com/Step-basic-python/fundamental-Python>
2020-11-05 17:04:32 +0700

    Learning Python Fundamental

---
+++ 08_Convert_feet _into_meters.py
@@ -0,0 +1,10 @@
+# Write a simple program to convert feet into meters
+
+# Prompt user for input value for feet
+feet = float(input('Enter a value for feet : '))
+
+# Convert feet into meters
+meters = feet * 0.305
+
+# Display the result
+print(f"{feet} feet is {meters} meters")
